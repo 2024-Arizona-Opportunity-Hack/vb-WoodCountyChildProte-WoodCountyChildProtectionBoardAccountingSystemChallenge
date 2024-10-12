554feadd923ed37ec648295f9f5c941463dd4dfc
benbousquet <bbousque@asu.edu>
2024-10-12 14:46:53 -0700

    prettified the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './index.css'
-import { useState, useEffect } from 'react'
-import { createClient, Session } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import "./index.css";
+import { useState, useEffect } from "react";
+import { createClient, Session } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 function App() {
   const supabaseUrl = "https://zyfeuwmhjyybilebrnnl.supabase.co";
@@ -14,27 +14,33 @@ function App() {
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
-    return () => subscription.unsubscribe()
-  }, [])
+    return () => subscription.unsubscribe();
+  }, []);
 
   if (!session) {
     return (
-      <div>
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />
+      <div className="max-w-5xl m-auto mt-10">
+        <h1 className="text-3xl ml-5">Login / Signup</h1>
+        <div className="max-w-4xl m-auto rounded-md border p-5 mt-10">
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={[]}
+          />
+        </div>
       </div>
-    )
-  }
-  else {
-    return (<div>Logged in!</div>)
+    );
+  } else {
+    return <div>Logged in!</div>;
   }
 }
 
