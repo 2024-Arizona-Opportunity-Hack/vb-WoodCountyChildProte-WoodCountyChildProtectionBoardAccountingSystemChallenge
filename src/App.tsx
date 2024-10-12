974e519d19cd0896ce20113e854d173b42f1bc23
vnleo <vincentleong.az@gmail.com>
2024-10-12 14:39:28 -0700

    added supabase auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient, Session } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 function App() {
   const supabaseUrl = "https://zyfeuwmhjyybilebrnnl.supabase.co";
@@ -6,7 +10,32 @@ function App() {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5ZmV1d21oanl5YmlsZWJybm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NjA4NTAsImV4cCI6MjA0NDMzNjg1MH0.3u-Tr1P__QeVdgj2t7lDpw3Z8nUzh-P_tkdqkMcCQIw";
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <div>
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />
+      </div>
+    )
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
 }
 
 export default App;
