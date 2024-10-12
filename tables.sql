ff4cdd3c2ddcf0c659476c2f9537d17c5ee2d17e
vnleo <vincentleong.az@gmail.com>
2024-10-12 14:09:21 -0700

    add sql tables

---
+++ tables.sql
@@ -0,0 +1,26 @@
+DROP TABLE transaction;
+DROP TABLE source;
+
+CREATE TABLE source (
+  id SERIAL PRIMARY KEY,
+  type TEXT NOT NULL,
+  name TEXT UNIQUE NOT NULL  
+);
+
+CREATE TABLE category (
+  id SERIAL PRIMARY KEY,
+  type TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE transaction (
+  id SERIAL PRIMARY KEY,
+  amount DECIMAL(10,2) NOT NULL,
+  source_id INT NOT NULL,
+  category_id INT NOT NULL,
+  date DATE NOT NULL,
+  description TEXT NOT NULL,
+  published_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  CONSTRAINT fk_source FOREIGN KEY(source_id) REFERENCES source(id),
+  CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES category(id)
+);
