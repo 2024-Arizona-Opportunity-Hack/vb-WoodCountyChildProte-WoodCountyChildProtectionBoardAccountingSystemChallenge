bf9a1f3a81af8b88027a0fef6c6f319617648455
benbousquet <bbousque@asu.edu>
2024-10-12 16:13:57 -0700

    fixed table types

--- types.ts
+++ types.ts
@@ -1,21 +1,23 @@
 type Category = {
-    id: number;
-    description: string;
-    type: string;
-}
+  id: number;
+  description: string;
+  type: string;
+};
 
 type Source = {
-    id: number;
-    type: string;
-    name: string;
-}
+  id: number;
+  type: string;
+  name: string;
+};
 
 type Transaction = {
-    id: number;
-    category_id: number;
-    source_id: number;
-    amount: number;
-    description: string;
-    date: string;
-    published_date: string;
-}
+  id: number;
+  category_id: number;
+  source_id: number;
+  amount: number;
+  description: string;
+  date: string;
+  published_date: string;
+};
+
+export { Category, Source, Transaction };
