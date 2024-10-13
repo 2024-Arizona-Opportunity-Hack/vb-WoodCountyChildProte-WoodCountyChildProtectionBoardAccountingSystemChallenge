f870374c85dddc153c74d9fca5d9823d31a15eec
benbousquet <bbousque@asu.edu>
2024-10-13 13:18:16 -0700

    added delete function for tables

--- src/components/SourceTable.tsx
+++ src/components/SourceTable.tsx
@@ -1,56 +1,58 @@
 import { useState, useEffect } from "react";
 import supabase from "../../supabase";
-import { Source } from "../../types"
+import { Source } from "../../types";
 
 function SourceTable() {
-    const [source, setSource] = useState<Source[] | null>([]);
+  const [source, setSource] = useState<Source[] | null>([]);
 
-    useEffect(() => {
-        getSource();
-    }, []);
+  useEffect(() => {
+    getSource();
+  }, []);
 
-    async function getSource() {
-        const { data } = await supabase.from("source").select();
-        //   console.log(data)
-        setSource(data);
+  async function getSource() {
+    const { data } = await supabase.from("source").select();
+    setSource(data);
+  }
+
+  async function deleteSource(sourceId: number) {
+    const { error } = await supabase.from("source").delete().eq("id", sourceId);
+    if (error === null) {
+      window.location.reload();
     }
+  }
 
-    return (
-        <div className="overflow-x-auto">
-            <table className="table table-xs">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>name</th>
-                        <th>type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {source?.map((source) => {
-
-                        return (
-                            <tr key={source.id}>
-                                <th>{source.id}</th>
-                                <td>{source.name}</td>
-                                <td>{source.type}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-                {/* <tfoot>
-                    <tr>
-                        <th>id</th>
-                        <th>amount</th>
-                        <th>source_id</th>
-                        <th>category_id</th>
-                        <th>date</th>
-                        <th>description</th>
-                        <th>published_date</th>
-                    </tr>
-                </tfoot> */}
-            </table>
-        </div>
-    );
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-xs">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {source?.map((source) => {
+            return (
+              <tr key={source.id}>
+                <th>{source.id}</th>
+                <td>{source.name}</td>
+                <td>{source.type}</td>
+                <td>
+                  <button
+                    className="btn btn-error"
+                    onClick={() => deleteSource(source.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default SourceTable
+export default SourceTable;
--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -9,111 +9,145 @@ import BalanceSheet from "../components/BalanceSheet";
 import DonorReceipt from "../components/DonorReceipt";
 import AddBudgetForm from "../components/AddBudgetForm";
 import BudgetChart from "../components/BudgetChart";
-import SpendDistributionChart from "../components/SpendDistributionChart";
 import IRSFiling from "../components/IRSFiling";
-import IncomeStatement from "../components/IncomeStatement";
 
 function Dashboard() {
-    return (
-        <div className="max-w-7xl m-auto">
-            <Navbar />
-            <h1 className="font-extrabold text-3xl p-10">Dashboard</h1>
-            <div className="p-5 rounded-xl border-2">
-                <h2 className="font-bold text-xl p-2">Add new data</h2>
-                <div role="tablist" className="tabs tabs-bordered">
-                    <input
-                        type="radio"
-                        name="forms"
-                        role="tab"
-                        className="tab"
-                        aria-label="Add Transactions"
-                    />
-                    <div role="tabpanel" className="tab-content p-10 max-w-6xl">
-                        <AddTransactionForm />
-                    </div>
+  return (
+    <div className="max-w-7xl m-auto">
+      <Navbar />
+      <h1 className="font-extrabold text-3xl p-10">Dashboard</h1>
+      <div className="p-5 rounded-xl border-2">
+        <h2 className="font-bold text-xl p-2">Add new data</h2>
+        <div role="tablist" className="tabs tabs-bordered">
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Transactions"
+          />
+          <div role="tabpanel" className="tab-content p-10 max-w-6xl">
+            <AddTransactionForm />
+          </div>
 
-                    <input
-                        type="radio"
-                        name="forms"
-                        role="tab"
-                        className="tab"
-                        aria-label="Add Categories"
-                        defaultChecked
-                    />
-                    <div role="tabpanel" className="tab-content p-10">
-                        <AddCategoryForm />
-                    </div>
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Categories"
+            defaultChecked
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddCategoryForm />
+          </div>
 
-                    <input
-                        type="radio"
-                        name="forms"
-                        role="tab"
-                        className="tab"
-                        aria-label="Add Sources"
-                    />
-                    <div role="tabpanel" className="tab-content p-10">
-                        <AddSourceForm />
-                    </div>
-                    <input
-                        type="radio"
-                        name="forms"
-                        role="tab"
-                        className="tab"
-                        aria-label="Add budget"
-                    />
-                    <div role="tabpanel" className="tab-content p-10">
-                        <AddBudgetForm />
-                    </div>
-                </div>
-            </div>
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Sources"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddSourceForm />
+          </div>
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add budget"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddBudgetForm />
+          </div>
+        </div>
+      </div>
 
-            {/* 2nd set of tabs - display data tables */}
-            <div className="p-5 rounded-xl border-2 mt-20">
-                <h2 className="font-bold text-xl p-3">Fund Accounting Table</h2>
-                <div role="tablist" className="tabs tabs-bordered">
-                    <input type="radio" name="tables" role="tab" className="tab" aria-label="Transactions" defaultChecked />
-                    <div role="tabpanel" className="tab-content p-10"><TransactionTable /></div>
+      {/* 2nd set of tabs - display data tables */}
+      <div className="p-5 rounded-xl border-2 mt-20">
+        <h2 className="font-bold text-xl p-3">Fund Accounting Table</h2>
+        <div role="tablist" className="tabs tabs-bordered">
+          <input
+            type="radio"
+            name="tables"
+            role="tab"
+            className="tab"
+            aria-label="Transactions"
+            defaultChecked
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <TransactionTable />
+          </div>
 
-                    <input
-                        type="radio"
-                        name="tables"
-                        role="tab"
-                        className="tab"
-                        aria-label="Sources"
-                    />
-                    <div role="tabpanel" className="tab-content p-10"><SourceTable /></div>
+          <input
+            type="radio"
+            name="tables"
+            role="tab"
+            className="tab"
+            aria-label="Sources"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <SourceTable />
+          </div>
 
-                    <input type="radio" name="tables" role="tab" className="tab" aria-label="Categories" />
-                    <div role="tabpanel" className="tab-content p-10"><CategoryTable /></div>
-                </div>
-            </div>
+          <input
+            type="radio"
+            name="tables"
+            role="tab"
+            className="tab"
+            aria-label="Categories"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <CategoryTable />
+          </div>
+        </div>
+      </div>
 
-            {/* 3rd set of tabs - donor management */}
-            <div className="p-5 rounded-xl border-2 mt-20">
-                <h2 className="font-bold text-xl p-3">Reports</h2>
-                <div role="tablist" className="tabs tabs-bordered">
-                    <input type="radio" name="reports" role="tab" className="tab" aria-label="Balance Sheet" defaultChecked />
-                    <div role="tabpanel" className="tab-content p-10"><BalanceSheet /></div>
+      {/* 3rd set of tabs - donor management */}
+      <div className="p-5 rounded-xl border-2 mt-20">
+        <h2 className="font-bold text-xl p-3">Reports</h2>
+        <div role="tablist" className="tabs tabs-bordered">
+          <input
+            type="radio"
+            name="reports"
+            role="tab"
+            className="tab"
+            aria-label="Balance Sheet"
+            defaultChecked
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <BalanceSheet />
+          </div>
 
-                    <input
-                        type="radio"
-                        name="reports"
-                        role="tab"
-                        className="tab"
-                        aria-label="Income Statement"
-                    />
-                    <div role="tabpanel" className="tab-content p-10"></div>
-                    {/* <IncomeStatement /> */}
+          <input
+            type="radio"
+            name="reports"
+            role="tab"
+            className="tab"
+            aria-label="Income Statement"
+          />
+          <div role="tabpanel" className="tab-content p-10"></div>
+          {/* <IncomeStatement /> */}
 
-                    <input type="radio" name="reports" role="tab" className="tab" aria-label="Donor Receipt" />
-                    <div role="tabpanel" className="tab-content p-10"><DonorReceipt /></div>
-                </div>
-            </div>
+          <input
+            type="radio"
+            name="reports"
+            role="tab"
+            className="tab"
+            aria-label="Donor Receipt"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <DonorReceipt />
+          </div>
+        </div>
+      </div>
 
-            {/*  4th set of tabs - irs filings */}
-            <div className="p-5 rounded-xl border-2 mt-20">
-                <h2 className="font-bold text-xl p-3">IRS Filings</h2>
-                {/* <div role="tablist" className="tabs tabs-bordered">
+      {/*  4th set of tabs - irs filings */}
+      <div className="p-5 rounded-xl border-2 mt-20">
+        <h2 className="font-bold text-xl p-3">IRS Filings</h2>
+        {/* <div role="tablist" className="tabs tabs-bordered">
                     <input type="radio" name="reports" role="tab" className="tab" aria-label="Balance Sheet" defaultChecked />
                     <div role="tabpanel" className="tab-content p-10"><BalanceSheet /></div>
 
@@ -128,16 +162,15 @@ function Dashboard() {
                     <input type="radio" name="reports" role="tab" className="tab" aria-label="Donor Receipt" />
                     <div role="tabpanel" className="tab-content p-10"><DonorReceipt /></div>
                 </div> */}
-                <IRSFiling />
-            </div>
+        <IRSFiling />
+      </div>
 
-            <div className="p-5 rounded-xl border-2 mt-20">
-                <h2 className="font-bold text-xl p-2">Budgets</h2>
-                <BudgetChart name={"rent"} used={123} available={1000} />
-            </div>
-            
-        </div >
-    );
+      <div className="p-5 rounded-xl border-2 mt-20">
+        <h2 className="font-bold text-xl p-2">Budgets</h2>
+        <BudgetChart name={"rent"} used={123} available={1000} />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
