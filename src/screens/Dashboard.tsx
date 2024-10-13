1763d53869c5a003c8275188c86abdc759961cfc
benbousquet <bbousque@asu.edu>
2024-10-12 19:33:57 -0700

    modified dashboard layout

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -7,40 +7,43 @@ function Dashboard() {
   return (
     <div className="max-w-7xl m-auto">
       <Navbar />
-      <p>Dashboard</p>
-      <div role="tablist" className="tabs tabs-bordered">
-        <input
-          type="radio"
-          name="forms"
-          role="tab"
-          className="tab"
-          aria-label="Add Transactions"
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          <AddTransactionForm />
-        </div>
+      <h1 className="font-extrabold text-3xl p-10">Dashboard</h1>
+      <div className="p-5 rounded-xl border-2">
+        <h2 className="font-bold text-xl p-2">Add new data</h2>
+        <div role="tablist" className="tabs tabs-bordered">
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Transactions"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddTransactionForm />
+          </div>
 
-        <input
-          type="radio"
-          name="forms"
-          role="tab"
-          className="tab"
-          aria-label="Add Categories"
-          defaultChecked
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          <AddCategoryForm />
-        </div>
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Categories"
+            defaultChecked
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddCategoryForm />
+          </div>
 
-        <input
-          type="radio"
-          name="forms"
-          role="tab"
-          className="tab"
-          aria-label="Add Funding Sources"
-        />
-        <div role="tabpanel" className="tab-content p-10">
-          <AddSourceForm />
+          <input
+            type="radio"
+            name="forms"
+            role="tab"
+            className="tab"
+            aria-label="Add Funding Sources"
+          />
+          <div role="tabpanel" className="tab-content p-10">
+            <AddSourceForm />
+          </div>
         </div>
       </div>
     </div>
