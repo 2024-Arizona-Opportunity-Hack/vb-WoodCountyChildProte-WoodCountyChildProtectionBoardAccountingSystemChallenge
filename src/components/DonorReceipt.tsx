48e63cfb970d55549a51af0b34a64ed166cc1af4
vnleo <vincentleong.az@gmail.com>
2024-10-12 19:50:35 -0700

    added tabs

--- src/components/DonorReceipt.tsx
+++ src/components/DonorReceipt.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import supabase from "../../supabase";
 
-function BalanceSheet() {
+function DonorReceipt() {
     const [assets, setAssets] = useState<number | undefined>()
     const [liabilities, setLiabilities] = useState<number | undefined>()
     const [equity, setEquity] = useState<number | undefined>()
@@ -23,31 +23,31 @@ function BalanceSheet() {
     }
 
     return (
-       <>
+        <div className="p-5 rounded-xl border-2 mt-5">
             <h1 className="text-3xl ml-5 text-center">Statement of Financial Position</h1>
             <div className="flex justify-center">
-                <div className="stats shadow">
-                    <div className="stat place-items-center">
-                        <div className="stat-title">Assets</div>
-                        <div className="stat-value">${assets}</div>
-                        {/* <div className="stat-desc">From January 1st to February 1st</div> */}
-                    </div>
+            <div className="stats shadow">
+                <div className="stat place-items-center">
+                    <div className="stat-title">Assets</div>
+                    <div className="stat-value">${assets}</div>
+                    {/* <div className="stat-desc">From January 1st to February 1st</div> */}
+                </div>
 
-                    <div className="stat place-items-center">
-                        <div className="stat-title">Liabilities</div>
-                        <div className="stat-value">${liabilities}</div>
-                        {/* <div className="stat-desc text-secondary">↗︎ 40 (2%)</div> */}
-                    </div>
+                <div className="stat place-items-center">
+                    <div className="stat-title">Liabilities</div>
+                    <div className="stat-value">${liabilities}</div>
+                    {/* <div className="stat-desc text-secondary">↗︎ 40 (2%)</div> */}
+                </div>
 
-                    <div className="stat place-items-center">
-                        <div className="stat-title">Net Assets (Equity)</div>
-                        <div className="stat-value">${equity}</div>
-                        {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
-                    </div>
+                <div className="stat place-items-center">
+                    <div className="stat-title">Net Assets (Equity)</div>
+                    <div className="stat-value">${equity}</div>
+                    {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
                 </div>
             </div>
-            </>
+            </div>
+        </div>
     );
 }
 
-export default BalanceSheet
+export default DonorReceipt
