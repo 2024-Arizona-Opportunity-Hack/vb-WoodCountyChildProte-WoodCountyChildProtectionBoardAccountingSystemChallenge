7949b292b8373ab4ee1ade579d495e828cd4dc57
vnleo <vincentleong.az@gmail.com>
2024-10-12 21:35:26 -0700

    donor/grant functionality

--- src/components/DonorReceipt.tsx
+++ src/components/DonorReceipt.tsx
@@ -2,50 +2,46 @@ import { useState, useEffect } from "react";
 import supabase from "../../supabase";
 
 function DonorReceipt() {
-    const [assets, setAssets] = useState<number | undefined>()
-    const [liabilities, setLiabilities] = useState<number | undefined>()
-    const [equity, setEquity] = useState<number | undefined>()
+    const [donors, setDonors] = useState<Donor[] | null>([]);
 
     useEffect(() => {
-        getTransaction();
+        getDonors();
     }, []);
 
-    async function getTransaction() {
-        const { data: assetsRes } = await supabase.from('transaction').select('amount.sum()').gte('amount', 0);;
-        // console.log(assets)
-        if (assetsRes) { setAssets(+assetsRes[0].sum); }
-        const { data: liabilitiesRes } = await supabase.from('transaction').select('amount.sum()').lte('amount', 0);
-        // console.log(liabilities)
-        if (liabilitiesRes) { setLiabilities(+liabilitiesRes[0].sum); }
-        const { data: equityRes } = await supabase.from('transaction').select('amount.sum()');
-        // console.log(equity)
-        if (equityRes) { setEquity(+equityRes[0].sum); }
+    async function getDonors() {
+        const { data } = await supabase.from('transaction').select(`
+		total_donations:amount.sum(),
+    ...source(name, type)
+	`)
+            .not('amount', 'lte', 0)
+        // .eq('type', 'donation')
+        // .not('type', 'eq', 'grant');
+
+        console.log(data)
+        setDonors(data);
     }
 
     return (
-        <div className="p-5 rounded-xl border-2 mt-5">
-            <h1 className="text-3xl ml-5 text-center">Statement of Financial Position</h1>
-            <div className="flex justify-center">
-            <div className="stats shadow">
-                <div className="stat place-items-center">
-                    <div className="stat-title">Assets</div>
-                    <div className="stat-value">${assets}</div>
-                    {/* <div className="stat-desc">From January 1st to February 1st</div> */}
-                </div>
+        <div className="overflow-x-auto">
+            <table className="table table-xs">
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>total donations</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {donors?.map((donors) => {
 
-                <div className="stat place-items-center">
-                    <div className="stat-title">Liabilities</div>
-                    <div className="stat-value">${liabilities}</div>
-                    {/* <div className="stat-desc text-secondary">↗︎ 40 (2%)</div> */}
-                </div>
-
-                <div className="stat place-items-center">
-                    <div className="stat-title">Net Assets (Equity)</div>
-                    <div className="stat-value">${equity}</div>
-                    {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
-                </div>
-            </div>
-            </div>
+                        return (
+                            <tr>
+                                <th>{donors.name}</th>
+                                <td>${donors.total_donations}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
